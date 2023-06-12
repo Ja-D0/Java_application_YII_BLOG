7c798d0e2613a4ec016c6b1cb9bfd12ed9d5a4a4
Чемерис Денис <denis.chemeris43@gmail.com>
2023-06-12 17:25:36 +0300

    Initial commit

--- app/src/main/java/com/example/blog/MainActivity.java
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -1,43 +1,26 @@
 package com.example.blog;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
     private TextView textView;
     private ImageButton imageButtonMenu;
-    private Button button;
+    private ImageButton button;
     private  DBController dbController;
     private int post_item_layout;
 
@@ -78,10 +61,10 @@ public class MainActivity extends AppCompatActivity {
        });
     }
     public void createPost(View view){
-        Intent intent = new Intent(this, MyPostsActivity.class);
+        Intent intent = new Intent(this, PostsDoActivity.class);
         startActivity(intent);
     }
-
+/*
     private void openProfile(){
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
@@ -91,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MyPostsActivity.class);
         startActivity(intent);
     }
+ */
     private void createMenu(){
         PopupMenu menu = MainMenu.createMainMenu(this, imageButtonMenu, textView);
         imageButtonMenu.setOnClickListener(new View.OnClickListener() {
@@ -103,14 +87,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()){
-                    case R.id.action_profile:{
-                        openProfile();
-                        return true;
-                    }
-                    case R.id.action_myPosts:{
-                        openMyPosts();
-                        return true;
-                    }
+                    //case R.id.action_profile:{
+                    //    openProfile();
+                    //    return true;
+                    //}
+                    //case R.id.action_myPosts:{
+                    //    openMyPosts();
+                    //    return true;
+                    //}
                     case R.id.action_refresh:{
                         dbController.deleteAll();
                         addPosts_toBD(HttpRequestController.get_posts());
@@ -133,6 +117,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         dbController.deleteAll();
-        Toast.makeText(this, "Удалено", Toast.LENGTH_SHORT).show();
     }
 }
